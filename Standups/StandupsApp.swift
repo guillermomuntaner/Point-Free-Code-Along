275c522cecd486c7aa97b6505ef3b1148fc4d33a
Guillermo Muntaner <guillermomp87@gmail.com>
2023-02-13 20:43:20 +0100

    Initial Commit

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Guillermo Muntaner on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
